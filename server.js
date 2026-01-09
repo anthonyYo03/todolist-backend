61d483937ed06f463e9ac1edff9a8fdb3104800e
anthonyYo03 <antoyou78@gmail.com>
2026-01-09 16:54:39 +0200

    Fix origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // 🔹 Middleware
 app.use(cors({
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
@@ -36,7 +36,7 @@ connectToDatabase();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"]
   }
 });
